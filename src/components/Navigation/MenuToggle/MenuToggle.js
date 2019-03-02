3d2d77260dfbc79d200bd366188750c4caca1ef2
Bob_chemist <bob_chemist@mail.ru>
2019-03-03 01:15:11 +0300

    added nav hamburger button

---
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './MenuToggle.module.css'
+
+const MenuToggle = props => {
+  const cls = [
+    classes.MenuToggle,
+    'fa',
+    props.isOpen ? 'fa-times open' : 'fa-bars',
+    props.isOpen ? classes.open : '',
+  ]
+  
+  return (
+    <i
+      className={cls.join(' ')}
+      onClick={props.onToggle}
+    />
+  )
+}
+
+export default MenuToggle;
