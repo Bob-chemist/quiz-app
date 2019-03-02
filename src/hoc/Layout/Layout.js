d0b54d330b35ee552f11a7f6301398a86214d401
Bob_chemist <bob_chemist@mail.ru>
2019-03-03 01:45:14 +0300

    added navigation menu

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.css'
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 
 class Layout extends Component {
 
@@ -16,6 +17,10 @@ class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
+
+        <Drawer 
+          isOpen={this.state.menu}
+        />
 
         <MenuToggle 
           onToggle={this.toggleMenuHandler}
