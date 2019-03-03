6bd375f652c9586f2d21df445ce67ca6d3b2d08c
Bob_chemist <bob_chemist@mail.ru>
2019-03-03 20:37:31 +0300

    Autentication page styling

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import classes from './Auth.module.sass'
+import Button from '../../components/UI/Button/Button' 
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className={classes.Auth}>
+        <div>
+          <h1>Authentication</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text"/>
+            <input type="text"/>
+            <Button 
+              type='success'
+              onClick={this.loginHandler}
+            >
+              Login
+            </Button>
+            <Button 
+              type='primary'
+              onClick={this.registerHandler}
+            >
+              Register
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
