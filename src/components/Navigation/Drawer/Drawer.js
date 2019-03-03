3a921ea55197c742dec1843d41a39b93e693a168
Bob_chemist <bob_chemist@mail.ru>
2019-03-03 18:29:49 +0300

    Links updated. Drawer closes on click

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,16 +1,32 @@
 import React, {Component} from 'react'
 import classes from './Drawer.module.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
+import { NavLink } from 'react-router-dom';
 
-const links = [1,2,3,];
+const links = [
+  {to: '/', label: 'List', exact: true,},
+  {to: '/auth', label: 'Authentication', exact: false,},
+  {to: '/quiz-creator', label: 'Create Quiz', exact: false,}, 
+];
 
 class Drawer extends Component {
+
+  clickHandler = () => {
+    this.props.onClose();
+  }
 
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a> 
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       )
     })
