e10336fc831414b28e2c5def04a36905412c8a21
Bob_chemist <bob_chemist@mail.ru>
2019-03-04 23:31:37 +0300

    Inputs for Create Quiz page are added

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+import classes from './QuizCreator.module.sass'
+import Button from '../../components/UI/Button/Button'
 
 export default class QuizCreator extends Component {
+
+  submitHandler = event => {
+    event.preventDefault();
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuizHandler = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Quiz Creator</h1>
+          <form onSubmit={this.submitHandler}>
+            <input type="text"/>
+            <hr/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <select name="" id=""></select>
+
+            <Button
+              type='primary'
+              onClick={this.addQuestionHandler}
+            >Add question</Button>
+            <Button
+              type='success'
+              onClick={this.createQuizHandler}
+            >Create quiz</Button>
+          </form>
+        </div>
+        
         
       </div>
     )
