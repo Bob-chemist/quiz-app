5e38b448faa2fd64cfa3580bf3cfedecf6eb8c92
Bob_chemist <bob_chemist@mail.ru>
2019-03-05 11:48:08 +0300

    Axios added, test request works

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import classes from './QuizList.module.sass'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios'
 
 export default class QuizList extends Component {
 
@@ -15,6 +16,13 @@ export default class QuizList extends Component {
           </NavLink>
         </li>
       )
+    })
+  }
+
+  componentDidMount() {
+    axios.get('https://react-quiz-14e28.firebaseio.com/quiz.json').then(response => {
+      console.log(response);
+      
     })
   }
 
